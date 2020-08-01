0ca114221d2bb1e3257337efcc1ad15002272f7f
Alix <liu.xueyong@qq.com>
2020-08-01 13:55:38 +0800

    Add files via upload

---
+++ js/to-date.js
@@ -0,0 +1,6 @@
+// 主页-日期显示
+        var date = new Date()
+        var y = date.getFullYear();
+        var m = date.getMonth()+1;
+        var d = date.getDate(); 
+        document.getElementById("ymd").innerHTML = "# "+y+" / "+m+" / "+d;
