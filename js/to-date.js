512142b7e49fd601f5f9583cfef0884bb3baef50
LXY <liu.xueyong@qq.com>
2020-08-15 13:33:56 +0800

    update

--- js/to-date.js
+++ js/to-date.js
@@ -3,5 +3,4 @@
         var y = date.getFullYear();
         var m = date.getMonth()+1;
         var d = date.getDate(); 
-        document.getElementById("ymd").innerHTML = "# "+y+" / "+m+" / "+d;
-        document.getElementById("date1").innerHTML = d;
+        document.getElementById("date1").innerHTML = d;       document.getElementById("ymd").innerHTML = "# "+y+" / "+m+" / "+d;
