47ccd72252e7d65e09b46efa07bc377de4e07e9a
LXY <liu.xueyong@qq.com>
2020-12-24 02:25:51 +0800

    Update by-lxy.js

--- js/by-lxy.js
+++ js/by-lxy.js
@@ -1,12 +1,13 @@
 // JavaScript - MADE BY LXY
 	var allmonths = ['JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC'];
     var date = new Date();//可以在date中指定日期转换
-    console.log("MADE BY LXY"),
-    console.log( 
-     date.getDate() + 
-     ' ' +
-     allmonths[date.getMonth()] +
-     ' ' + 
-     date.getFullYear()
-    );
+        console.log("I HAVE A DREAM\n-"),
+        console.log("MADE BY LXY"),
+        console.log( 
+           date.getDate() + 
+                ' ' +
+                allmonths[date.getMonth()] +
+                ' ' + 
+           date.getFullYear()
+        );
     document.getElementById('date2').innerText = allmonths[date.getMonth()]; 
