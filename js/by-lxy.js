4daf24c6568493b3ed20cc3e46e1697bb5b5a932
LXY <liu.xueyong@qq.com>
2020-12-24 01:16:10 +0800

    Update

--- js/by-lxy.js
+++ js/by-lxy.js
@@ -1,6 +1,7 @@
-// JavaScript Document
+// JavaScript - MADE BY LXY
 	var allmonths = ['JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC'];
     var date = new Date();//可以在date中指定日期转换
+    console.log("MADE BY LXY"),
     console.log( 
      date.getDate() + 
      ' ' +
